961a9c0494747b02a770d46dc15a0b724280c598
amutheezan <amux93@yahoo.com>
2015-12-05 23:33:47 +0530

    modified files

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistantExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistantExpenseManager.java
@@ -1,17 +1,27 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 
 /**
  * Created by amutheezan on 12/5/15.
  */
 public class PersistantExpenseManager extends ExpenseManager {
-    /***
-     * This method should be implemented by the concrete implementation of this class. It will dictate how the DAO
-     * objects will be initialized.
-     */
+    public PersistantExpenseManager(){
+
+        try {
+            setup();
+        } catch (ExpenseManagerException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void setup() throws ExpenseManagerException {
-        
+
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
+
     }
 }
