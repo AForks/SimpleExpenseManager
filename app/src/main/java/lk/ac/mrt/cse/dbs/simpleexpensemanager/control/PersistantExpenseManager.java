cb08209648465b5c23f91240e0fba56e602541f7
amutheezan <amux93@yahoo.com>
2015-12-06 10:55:37 +0530

    modified PersistantExpenseManager

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistantExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistantExpenseManager.java
@@ -1,6 +1,10 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistantAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistantTransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 
 /**
@@ -17,7 +21,11 @@ public class PersistantExpenseManager extends ExpenseManager {
     }
     @Override
     public void setup() throws ExpenseManagerException {
+        TransactionDAO PersistantTransactionDAO = new PersistantTransactionDAO(null);
+        setTransactionsDAO(PersistantTransactionDAO) ;
 
+        AccountDAO PersistantAccountDAO = new PersistantAccountDAO(null);
+        setAccountsDAO(PersistantAccountDAO);
         Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
         Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
         getAccountsDAO().addAccount(dummyAcct1);
